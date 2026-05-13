c411586a52a04bc22b72cdae170b6d6941b9a8f6
aylindmm <aylindmm@gmail.com>
2026-05-13 11:38:48 -0600

    add download page

---
+++ pages/4_Download.py
@@ -0,0 +1,32 @@
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+# Show the page title and description.
+st.set_page_config(page_title="Download")
+st.title("Data Download")
+st.write(
+    """
+Click here to download the data:.
+"""
+)
+#st.cache_data.clear()
+# Load the data from a CSV. We're caching this so it doesn't reload every time the app
+# reruns (e.g. if the user interacts with the widgets).
+@st.cache_data
+def load_data():
+    df = pd.read_csv("data/articles.tsv", sep="\t")
+    return df
+
+
+df = load_data()
+
+# Convert dataframe to CSV
+csv = df.to_csv(index=False).encode('utf-8')
+
+st.download_button(
+    label="Download data as CSV",
+    data=csv,
+    file_name='articles.csv',
+    mime='text/csv',
+)
