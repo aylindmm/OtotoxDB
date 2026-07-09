bc139239589d60831dfe56dfb135dfd940a1f819
Aylin <69055072+aylindmm@users.noreply.github.com>
2026-07-09 03:11:46 +0000

    fix typo

--- pages/6_About.py
+++ pages/6_About.py
@@ -6,7 +6,7 @@ import streamlit as st
 st.set_page_config(page_title="About")
 st.title("About this page")
 
-st.write("This page was developed by the Biophysicochemistry Department of Universidad Autónoma Metropolitana-Cuajimalpa in colaboration with DIFACQUIM at Faculty of Chemistry, National Autonomous University of Mexico")
+st.write("This page was developed by the Biophysicochemistry Department of Universidad Autónoma Metropolitana-Cuajimalpa in collaboration with DIFACQUIM at the Faculty of Chemistry, National Autonomous University of Mexico.")
 
 st.write("If you use the database, please cite: ")
 st.write("del Moral-Morales A, Arguello‐Camarillo J, Castañón Bello JY, Chavez-Romero A, Medina-Franco JL, Nájera H and Pérez-Hernández G (2026) Mapping the otoactive landscape: an LLM-aided extraction of compounds and their targets. Front. Drug Discov. 6:1834905. doi: 10.3389/fddsv.2026.1834905")
